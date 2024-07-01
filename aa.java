4c11227ef25b87048357d9325c31b6c2b3155fc5
Aniket Practice <waghmareaniket150@gmail.com>
2024-07-01 22:21:05 +0530

    New file added

---
+++ aa.java
@@ -0,0 +1,8 @@
+package AasthaGit;
+
+public class aa {
+    public static void main(String[] args) {
+        System.out.println("Hello Aastha💕");
+        System.out.println("Betu😚");
+    }
+}
---
+++ aaa.java
@@ -0,0 +1,7 @@
+package AasthaGit;
+
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println("Betu😚");
+    }
+}
